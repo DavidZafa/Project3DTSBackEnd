1bfc39a55065480fa6f83710f3f71ca0770c72bc
“Stephanie <stephanie.techarmour@gmail.com>
2018-08-23 15:08:47 -0400

    fixed user controller and pictures populate correctly

--- controllers/user.js
+++ controllers/user.js
@@ -1,20 +1,19 @@
-const mongoose = require("../models/User")
-const User = mongoose.model('User')
-const config = require("../config/config")
-const jwt = require('jwt-simple')
-const Animals = require("../models/Animals")
-const News = require("../models/News")
+const mongoose = require("../models/User");
+const User = mongoose.model("User");
+const config = require("../config/config");
+const jwt = require("jwt-simple");
+const Animals = require("../models/Animals");
+const News = require("../models/News");
 
 module.exports = {
   show: (req, res) => {
-
     User.findOne({ _id: req.params.id })
-      .populate("animals")
-      .then(res => {
-        res.json();
+      .populate("animalList")
+      .then(user => {
+        res.json(user);
       });
   },
-  createLogin: (res, req) => {
+  createLogin: (req, res) => {
     if (req.body.email && req.body.password) {
       User.findOne({ email: req.body.email }).then(user => {
         if (user) {
@@ -26,37 +25,9 @@ module.exports = {
             res.json({
               token: token
             });
-
-        User.findOne({_id: req.params.id})
-        .populate("animalList")
-        .then(user => {
-            res.json(user)
-        })
-    }
-  }
-})
-    }},
-
-    createLogin: (req,res) => {
-      if (req.body.email && req.body.password) {
-        User.findOne({ email: req.body.email }).then(user => {
-          if (user) {
-            if (user.password === req.body.password) {
-              var payload = {
-                id: user.id
-              }
-              var token = jwt.encode(payload, config.jwtSecret)
-              res.json({
-                token: token
-              })
-            } else {
-              res.sendStatus(401)
-            }
-
           } else {
             res.sendStatus(401);
           }
-
         } else {
           res.sendStatus(401);
         }
@@ -71,9 +42,7 @@ module.exports = {
         email: req.body.email,
         password: req.body.password
       };
-      User.findOne({
-        email: req.body.email
-      }).then(user => {
+      User.findOne({ email: req.body.email }).then(user => {
         if (!user) {
           User.create(newUser).then(user => {
             if (user) {
@@ -84,67 +53,38 @@ module.exports = {
               res.json({
                 token: token
               });
-
-        })
-      } else {
-        res.sendStatus(401)
-      }
-        },
-    createSignUp: (req, res) => {
-      if (req.body.email && req.body.password) {
-        let newUser = {
-          email: req.body.email,
-          password: req.body.password
-        }
-        User.findOne({ email: req.body.email })
-          .then((user) => {
-            if (!user) {
-              User.create(newUser)
-                .then(user => {
-                  if (user) {
-                    var payload = {
-                      id: user.id
-                    }
-                    var token = jwt.encode(payload, config.jwtSecret)
-                    res.json({
-                      token: token
-                    })
-                  } else {
-                    res.sendStatus(401)
-                  }
-                })
             } else {
-              res.sendStatus(401)
+              res.sendStatus(401);
             }
-          })
-      } else {
-        res.sendStatus(401)
-      }
-    },
-    addAnimal: (req, res) => {
-      User.findById(req.params.id)
+          });
+        } else {
+          res.sendStatus(401);
+        }
+      });
+    } else {
+      res.sendStatus(401);
+    }
+  },
+  addAnimal: (req, res) => {
+    User.findById(req.params.id).then(user => {
+      Animals.findOne({ name: req.body.animalname }).then(animal => {
+        user.animalList.push(animal);
+        user.save();
+        console.log(user);
+      });
+    });
+  },
+  removeAnimal: (req, res) => {
+    User.findOne({ _id: req.params.id })
       .then(user => {
-        Animals.findOne({name: req.body.animalname})
-        .then(animal => {
-          user.animalList.push(animal)
-          user.save()
-          console.log(user)
-        })
-
-      })
-    },
-    removeAnimal: (req, res) => {
-      User.findOne({_id: req.params.id})
-      .then(user => {
-        Animals.findOne({name: req.body.animalname})
-        .then(animal => {
-          user.animalList.pull(animal)
-          user.save()
-          console.log(user)
-        })
+        Animals.findOne({ name: req.body.animalname }).then(animal => {
+          user.animalList.pull(animal);
+          user.save();
+          console.log(user);
+        });
       })
       .catch(err => {
-        console.log(err)
-      })
-    }
-}
+        console.log(err);
+      });
+  }
+};
