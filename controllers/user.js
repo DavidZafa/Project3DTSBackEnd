8e6b2a2631459c38f5da0bfbe88271162a11819a
“Stephanie <stephanie.techarmour@gmail.com>
2018-08-23 14:39:24 -0400

    in user controller under user create changed newuser.id to user.id

--- controllers/user.js
+++ controllers/user.js
@@ -10,35 +10,35 @@ const User = mongoose.model("User");
 
 module.exports = {
   show: (req, res) => {
-      User.findOne({_id: req.params.id})
+    User.findOne({ _id: req.params.id })
       .populate("animals")
       .then(res => {
-          res.json()
-      })
+        res.json();
+      });
   },
-  createLogin: (res,req) => {
+  createLogin: (res, req) => {
     if (req.body.email && req.body.password) {
       User.findOne({ email: req.body.email }).then(user => {
         if (user) {
           if (user.password === req.body.password) {
             var payload = {
               id: user.id
-            }
-            var token = jwt.encode(payload, config.jwtSecret)
+            };
+            var token = jwt.encode(payload, config.jwtSecret);
             res.json({
               token: token
-            })
+            });
           } else {
-            res.sendStatus(401)
+            res.sendStatus(401);
           }
         } else {
-          res.sendStatus(401)
+          res.sendStatus(401);
         }
-      })
+      });
     } else {
-      res.sendStatus(401)
+      res.sendStatus(401);
     }
-      },
+  },
   createSignUp: (req, res) => {
     if (req.body.email && req.body.password) {
       let newUser = {
@@ -52,7 +52,7 @@ module.exports = {
           User.create(newUser).then(user => {
             if (user) {
               var payload = {
-                id: newUser.id
+                id: user.id
               };
               var token = jwt.encode(payload, config.jwtSecret);
               res.json({
