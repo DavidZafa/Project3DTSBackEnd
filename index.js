7eef866e47e0f7cc0e573173850d47276c3cb9b4
“Stephanie <stephanie.techarmour@gmail.com>
2018-09-06 14:07:38 -0400

    cors attempt 3

--- index.js
+++ index.js
@@ -26,28 +26,16 @@ const app = express();
 
 app.use(cors());
 app.use(function(req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
     "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
   );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
   next();
 });
+
 app.use(parser.json());
 app.use(passport.initialize());
 app.use(require("./routes/index"));
