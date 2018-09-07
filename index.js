b863ff71a5bf927342e9aab59c973a7ea4513a31
Tasha Mitchell <tamitchell14@gmail.com>
2018-09-07 14:04:30 -0400

    updated master

--- index.js
+++ index.js
@@ -1,44 +1,25 @@
-// const express = require('express')
-// const cors = require('cors')
-// const bodyParser = require('body-parser');
-// const passport = require('./config/passport')()
-// const app = express()
-
-// app.use(cors())
-// app.use(bodyParser.json())
-// app.use(bodyParser.urlencoded({     
-//     extended: true
-//   })); 
-// app.use(passport.initialize())
-// app.use(require('./routes/index.js'))
-
-// app.set('port', process.env.PORT || 3001)
-// app.listen(app.get('port'), () => {
-//     console.log('GET READY TO GET RICKETY RICKETY RECKED - on port 3001')
-// })
-
-
-
-
-
 /* FROM LAB START*/
-const express = require('express')
-const parser = require('body-parser')
-const cors = require('cors')
-const passport = require('./config/passport')()
+const express = require("express");
+const parser = require("body-parser");
+const cors = require("cors");
+const passport = require("./config/passport")();
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(parser.json())
-app.use(passport.initialize())
+var corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
-app.use(require('./routes/index'))
+app.use(cors(corsOptions));
+app.use(parser.json());
+app.use(passport.initialize());
 
-app.set('port', process.env.PORT || 3001)
-app.listen(app.get('port'), () => {
-    console.log('GET READY TO GET RICKETY RICKETY RECKED - on port 3001')
-})
+app.use(require("./routes/index"));
 
+app.set("port", process.env.PORT || 3001);
+app.listen(app.get("port"), () => {
+  console.log("GET READY TO GET RICKETY RICKETY RECKED - on port 3001");
+});
 
 /* FROM LAB END*/
